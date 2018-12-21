719e6ebf9fcaf606127de6ad08602c0f16e4240c
Roberto Hernandez <rh@fusion52.com>
2018-12-20 21:12:23 -0800

    updated res message

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const app = express();
 const foo = 'jdjdj';
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hello: 'world' });
 });
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+// ➜  server git:(master) heroku create
+// Creating app... done, ⬢ sheltered-oasis-11786
+// https://sheltered-oasis-11786.herokuapp.com/ | https://git.heroku.com/sheltered-oasis-11786.git
+// 
+// git push heroku master
+// heroku open
+// heroku logs
+// 
+// 
