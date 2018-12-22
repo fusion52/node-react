aeef4e3f899368421d445ae98c29461c5c7708d1
Roberto Hernandez <rh@fusion52.com>
2018-12-21 22:24:27 -0800

    Section 5 - videos 45 to 49.

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// productin keys here! - DO COMMIT!
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
