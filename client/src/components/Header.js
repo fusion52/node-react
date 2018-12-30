0414cdc4d0f9d5a58e58a1c4d135c76058a8c121
Roberto Hernandez <rh@fusion52.com>
2018-12-29 21:51:58 -0800

    v74. UI header with CSS.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,18 @@ class Header extends Component {
   render(){
     return (
       <div>
-        Header
+        <nav>
+          <div className="nav-wrapper">
+            <a className="left brand-logo" href="/">
+              Emaily
+            </a>
+            <ul className="right">
+              <li>
+                <a href="/">Login With Google</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
