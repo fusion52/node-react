2b5a16efac59aa17c3fe3ad3a71274a31cd02f7a
Roberto Hernandez <rh@fusion52.com>
2018-12-31 13:31:23 -0800

    v101-103. body-paser install.  create charge object.  console.log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 // const passportConfig = require('./services/passport');
 require('./models/User');
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json()); //
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
