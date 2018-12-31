10c975a9ddaf52fc93257fb0b86a838aa9f1c0e6
Roberto Hernandez <rh@fusion52.com>
2018-12-31 14:30:22 -0800

    v105-106. requiring authentication, route-specific middlewares.

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,9 +1,13 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
-
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    // if (!req.user) {
+    //   return res.status(401).send({ error: 'You must log in!' });
+    // }
+
     // console.log(req.body);
     const charge = await stripe.charges.create({
       amount: 500,
