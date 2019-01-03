b4a7eabdd754b7964c45f9dd71ecc9b8c5de89a0
Roberto Hernandez <rh@fusion52.com>
2019-01-02 20:45:47 -0800

    v126-131. Creating Mailers, identifying unique users, sendgrid setup, mailer setup, mailer in use.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -19,5 +21,8 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,1 @@
+module.exports = survey => `<div>${  survey.body  }</div>`;
