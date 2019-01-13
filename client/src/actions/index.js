a8205845473162d8fce100a17c50730d660e2175
Roberto Hernandez <rh@fusion52.com>
2019-01-12 17:28:27 -0800

    v140-168. Back to the Client. Survey creation, Material Icons, Navigation with the Link Tag, SurveyNew Form, Purpose of Redux Form, ReduxForm Helper, Redux Form in Practice, Custom Field Components, Wiring up Custom Fields, Fields, Styling the Form, Form Validation, Showing Validation Errors, Generalizing Field Validation, Validating Emails, Invalide Emails, ETC..., Posting to Surveys, Redirect on Submit.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { FETCH_USER } from './types';
+import axios from 'axios';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // one express, can shorten code. Action Creater
 export const fetchUser = () => async (dispatch) => {
@@ -12,4 +12,17 @@ export const handleToken = (token) => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  history.push('/surveys');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing'; // header component
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
+
+// Dummy code
+// const Dashboard = () => <h2>Dashboard</h2> 
+// const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
   // lifecycle method, not componentWillMount
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchUser();
   }
 
-  render(){
+  render() {
     return (
       <div className="container">
         <BrowserRouter>
@@ -22,12 +26,12 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
-            <Route exact path="/surveys/new" component={SurveyNew} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
     );
   }
-};
+}
 
 export default connect(null, actions)(App);
